a9b397ffac99fa6969fe1f463d55ca76744f5682
DalJ214 <joefazluna@gmail.com>
2024-10-03 11:28:13 -0500

    Commiting LoopExercise

---
+++ LoopExercise/src/main/java/com/pluralsight/LoopingExcercises.java
@@ -0,0 +1,16 @@
+package com.pluralsight;
+
+public class LoopingExcercises {
+
+    public static void main(String[] args){
+
+        int count = 0;
+
+        while(count <= 5){
+            System.out.println("I Love Java " );
+            count++;
+        }
+
+    }
+
+}
